d40a379b35dd0a1c703280fd77840da19948f16d
kitaisreal <kitaetoya@gmail.com>
2017-05-21 01:07:57 +0300

    add SpringMVCWeb

---
+++ src/main/java/spring/config/AppConfig.java
@@ -0,0 +1,7 @@
+package spring.config;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ src/main/java/spring/homeController.java
@@ -0,0 +1,15 @@
+package spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class homeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String home(){
+        return "HelloSUKA";
+    }
+}
